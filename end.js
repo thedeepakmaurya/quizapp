af526faa7fbd6db1943fc5eb067e6b6837585bb6
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-04-13 13:00:21 +0530

    highscores added to localstorage

--- end.js
+++ end.js
@@ -2,6 +2,12 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+localStorage.setItem('highScores', JSON.stringify([]));
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -10,4 +16,18 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault()
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => {
+        return b.score - a.score;
+    })
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+
+    highScores.splice(5);
 }
