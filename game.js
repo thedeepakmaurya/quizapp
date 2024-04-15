51e68a95478c40ad25ba83c7a1116fccab6c7496
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-04-15 14:43:02 +0530

    loader added

--- game.js
+++ game.js
@@ -3,6 +3,10 @@ const choices = document.getElementsByClassName("choice-text");
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -25,7 +29,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple")
 
            const answerChoices = [...loadedQuestion.incorrect_answers];
            formattedQuestion.answer = Math.floor(Math.random()*3) +1;
-           answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answers);
+           answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
 
            answerChoices.forEach((choice, index) => {
             formattedQuestion["choice" + (index + 1)] = choice;
@@ -96,6 +100,9 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
+
 }
 
 getNewQuestion = () => {
